a6d9368058e1a89174f51725271bce799817d441
PIYUSHSENAPATI <83014608+PIYUSHSENAPATI@users.noreply.github.com>
2021-08-08 10:40:32 +0530

    Add files via upload

---
+++ project 44/Treasure-Hunt-master/js/Security.js
@@ -0,0 +1,35 @@
+class Security {
+
+    constructor(){
+
+        // Add code to create the input and button elements
+        this.input1=createInput();
+        this.button1=createButton('check');
+        this.input2=createInput();
+        this.button2=createButton('check');
+        this.input3=createInput();
+        this.button3=createButton('check');
+
+
+
+
+            
+
+
+    }
+
+    display(){
+
+        // Add code to make the buttons function as expected
+        this.input1.position(100,80);
+        this.button1.position(100,120);
+        this.input2.position(100,300);
+        this.button2.position(100,350);
+        this.input3.position(700,200);
+        this.button3.position(700,250);
+
+
+
+
+    }
+}
